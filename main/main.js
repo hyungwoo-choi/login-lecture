36d94078f713822d902b80de20e20b426197a613
hyungwoo-choi <tinbed122@gmail.com>
2024-01-26 17:47:57 +0900

    폴더구조 최적화

--- main/main.js
+++ main/main.js
@@ -5,11 +5,11 @@ const express = require('express');
 const app = express();
 
 //앱 셋팅
-app.set("views","./views");
+app.set("views","./src/views");
 app.set("view engine", "ejs");
 
 //미들웨어, use => 미들웨어를 동록해주는 메서드
-const home = require("./routes/home"); //  개발자가 자체적으로 만든 모듈을 쓰는것임.
+const home = require("./src/routes/home"); //  개발자가 자체적으로 만든 모듈을 쓰는것임.
 app.use("/", home);
 
 module.exports = app;
