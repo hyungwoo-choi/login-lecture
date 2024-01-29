5b2827d2810cacdacc9bd90c373739256d63bd91
hyungwoo-choi <tinbed122@gmail.com>
2024-01-30 01:38:56 +0900

    로그인 화면 꾸미기

--- main/src/routes/home/home.ctrl.js
+++ main/src/routes/home/home.ctrl.js
@@ -1,17 +1,48 @@
 "use strict";
 
+const User = require("../../models/User");
 
-//실제기능을 하는 부분임
-const hello = (req, res) => {
-    res.render("home/index"); // 앱 셋팅에서 뷰로 경로를 지정해주었음
-}
-const login = (req, res) =>{
-    res.render("home/login");
-}
+
+const output = {
+    hello : (req, res) => {
+        res.render("home/index"); // 앱 셋팅에서 뷰로 경로를 지정해주었음
+    },
+    login : (req, res) =>{
+        res.render("home/login");
+    },
+};
+
+
+const process = {
+    login : (req, res) => {
+        const user = new User(req.body);
+        const response = user.login();
+        return res.json(response); 
+
+        // // const id = req.body.id,
+        // //   psword = req.body.psword;
+
+        // // const users = UserStorage.getUsers("id", "psword");
+            
+        // // const response = {};
+
+        // // if (users.id.includes(id)) {
+        // //     const idx = users.id.indexOf(id);
+        // //     if(users.psword[idx] === psword) {
+        // //         response.success = true;
+        // //         return res.json(response);
+        // //     }
+        // // }
+        // // response.success = false;
+        // // response.msg = "로그인에 실패하였습니다.";
+        // // return res.json(response);
+    },
+};
+
 
 
 //기능 하나당 객체로 뺄 수 있음.
 module.exports = { // 키를 하나만 입력하면 키와같은 밸류를 입력해줌
-    hello,
-    login,
+    output,
+    process,
 };
--- main/src/routes/home/index.js
+++ main/src/routes/home/index.js
@@ -4,7 +4,8 @@ const express = require("express");
 const router = express.Router();
 const ctrl = require("./home.ctrl");
 
-router.get("/", ctrl.hello);
-router.get("/login", ctrl.login);
+router.get("/", ctrl.output.hello);
+router.get("/login", ctrl.output.login);
+router.post("/login", ctrl.process.login);
 
 module.exports = router; // 바깥으로 보내줘야됨
